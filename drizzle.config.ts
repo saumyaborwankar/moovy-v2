6df8d9fa7f52ead42956fd5f382eddc3c2de7192
Saumya Borwankar <sborwankar@braidit.com>
2024-09-13 11:20:37 -0500

    ready to deploy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,16 @@ export default defineConfig({
   schema: "./lib/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DB_URL!,
+    // url: process.env.DB_URL!,
+    password: process.env.DB_PASSWORD!,
+    user: process.env.DB_USERNAME!,
+    host: process.env.DB_URL!,
+    database: process.env.DB_NAME!,
+    ssl: {
+      rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
+    },
+    port: parseInt(process.env.DB_PORT!),
   },
   verbose: true,
-  strict: true,
+  // strict: true,
 });
