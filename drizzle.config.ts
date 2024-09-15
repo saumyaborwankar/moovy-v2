2f31b106b87174002c91ad2870dfa1d8b206e41e
Saumya Borwankar <sborwankar@braidit.com>
2024-09-15 09:41:26 -0500

    db config sortd

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,31 @@
 import { defineConfig } from "drizzle-kit";
-export default defineConfig({
-  schema: "./lib/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    // url: process.env.DB_URL!,
-    password: process.env.DB_PASSWORD!,
-    user: process.env.DB_USERNAME!,
-    host: process.env.DB_URL!,
-    database: process.env.DB_NAME!,
-    ssl: {
-      rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
-    },
-    port: parseInt(process.env.DB_PORT!),
+
+const localConfig = {
+  url: process.env.DB_URL_LOCAL!,
+};
+const testConfig = {
+  password: process.env.DB_PASSWORD!,
+  user: process.env.DB_USERNAME!,
+  host: process.env.DB_URL!,
+  database: process.env.DB_NAME!,
+  ssl: {
+    rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
   },
-  verbose: true,
-  // strict: true,
-});
+  port: parseInt(process.env.DB_PORT!),
+};
+const config =
+  process.env.STAGE === "local"
+    ? defineConfig({
+        schema: "./lib/db/schema.ts",
+        dialect: "postgresql",
+        dbCredentials: localConfig,
+        verbose: true,
+      })
+    : defineConfig({
+        schema: "./lib/db/schema.ts",
+        dialect: "postgresql",
+        dbCredentials: testConfig,
+        verbose: true,
+      });
+
+export default config;
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,30 +3,24 @@ import pg from "pg";
 import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-// import { drizzle } from 'drizzle-orm/aws-data-api/pg';
-// import { RDSDataClient } from '@aws-sdk/client-rds-data';
-// import { fromIni } from '@aws-sdk/credential-providers';
-// const rdsClient = new RDSDataClient({
-//     credentials: fromIni({ profile: process.env['PROFILE'] }),
-//     region: 'ap-south-1',
-// });
-// const db = drizzle(rdsClient, {
-//   database: process.env['DATABASE']!,
-//   secretArn: process.env['SECRET_ARN']!,
-//   resourceArn: process.env['RESOURCE_ARN']!,
-// });
-
-const pool = new pg.Pool({
-  ssl: {
-    rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
-  },
-  password: process.env.DB_PASSWORD!,
-  user: process.env.DB_USERNAME!,
-  host: process.env.DB_URL!,
-  database: process.env.DB_NAME!,
-  port: parseInt(process.env.DB_PORT!),
-  // connectionString: process.env.DB_URL,
-});
-const db = drizzle(pool, { schema }) as NodePgDatabase<typeof schema>;
+let db: NodePgDatabase<typeof schema>;
+if (process.env.STAGE === "local") {
+  const pool = new pg.Pool({
+    connectionString: process.env.DB_URL_LOCAL,
+  });
+  db = drizzle(pool, { schema }) as NodePgDatabase<typeof schema>;
+} else {
+  const pool = new pg.Pool({
+    ssl: {
+      rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
+    },
+    password: process.env.DB_PASSWORD!,
+    user: process.env.DB_USERNAME!,
+    host: process.env.DB_URL!,
+    database: process.env.DB_NAME!,
+    port: parseInt(process.env.DB_PORT!),
+  });
+  db = drizzle(pool, { schema }) as NodePgDatabase<typeof schema>;
+}
 
 export default db;
